c43f2028b9a4c6c8623dca42759d2f3036ebc659
Nema Dean <nemad@stat.washington.edu>
2009-02-19 00:00:00 +0000

    version 1.1

--- R/clustvarsel.R
+++ R/clustvarsel.R
@@ -1,7 +1,7 @@
 "clustvarsel" <-
 function(X,G,emModels1=c("E","V"),emModels2=c("EII","VII","EEI","VEI","EVI","VVI","EEE","EEV","VEV","VVV"),samp=FALSE,sampsize=2000,allow.EEE=TRUE,forcetwo=TRUE,search="greedy",upper=0,lower=-10,itermax=100)
 {
-require(mclust)
+require(mclust02)
 #Check whether there are variable names to identify selected variables
 ifelse(is.null(colnames(X)),colnames(X)<-1:ncol(X),colnames(X)<-colnames(X))
 #Apply varselection function depending on whether subsampling is required, samp=T or greedy or headlong search is required
